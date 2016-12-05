3f64d01c5a8530c1529b9d0aca1de309aa05045e
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-12-05 14:16:06 -0500

    Initial Commit

---
+++ RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chooseLabel = new System.Windows.Forms.Label();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.scissorsButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.winsLabel = new System.Windows.Forms.Label();
+            this.lossesLabel = new System.Windows.Forms.Label();
+            this.tiesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chooseLabel
+            // 
+            this.chooseLabel.BackColor = System.Drawing.Color.DarkRed;
+            this.chooseLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chooseLabel.ForeColor = System.Drawing.Color.White;
+            this.chooseLabel.Location = new System.Drawing.Point(12, 0);
+            this.chooseLabel.Name = "chooseLabel";
+            this.chooseLabel.Size = new System.Drawing.Size(107, 98);
+            this.chooseLabel.TabIndex = 0;
+            this.chooseLabel.Text = "Choose Your Move";
+            this.chooseLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // rockButton
+            // 
+            this.rockButton.BackgroundImage = global::RockPaperScissors.Properties.Resources.rock;
+            this.rockButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rockButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rockButton.Location = new System.Drawing.Point(40, 128);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(50, 50);
+            this.rockButton.TabIndex = 1;
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.rockButton_Click);
+            // 
+            // paperButton
+            // 
+            this.paperButton.BackgroundImage = global::RockPaperScissors.Properties.Resources.paper;
+            this.paperButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.paperButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.paperButton.Location = new System.Drawing.Point(40, 199);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(50, 50);
+            this.paperButton.TabIndex = 2;
+            this.paperButton.UseVisualStyleBackColor = true;
+            this.paperButton.Click += new System.EventHandler(this.paperButton_Click);
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.BackgroundImage = global::RockPaperScissors.Properties.Resources.scissors;
+            this.scissorsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scissorsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scissorsButton.Location = new System.Drawing.Point(40, 269);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(50, 50);
+            this.scissorsButton.TabIndex = 3;
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            this.scissorsButton.Click += new System.EventHandler(this.scissorsButton_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.DarkRed;
+            this.scoreLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(569, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(107, 98);
+            this.scoreLabel.TabIndex = 6;
+            this.scoreLabel.Text = "Fighting Record";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // winsLabel
+            // 
+            this.winsLabel.BackColor = System.Drawing.Color.DarkRed;
+            this.winsLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winsLabel.ForeColor = System.Drawing.Color.White;
+            this.winsLabel.Location = new System.Drawing.Point(569, 127);
+            this.winsLabel.Name = "winsLabel";
+            this.winsLabel.Size = new System.Drawing.Size(107, 51);
+            this.winsLabel.TabIndex = 7;
+            this.winsLabel.Text = "Wins: 0";
+            this.winsLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // lossesLabel
+            // 
+            this.lossesLabel.BackColor = System.Drawing.Color.DarkRed;
+            this.lossesLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lossesLabel.ForeColor = System.Drawing.Color.White;
+            this.lossesLabel.Location = new System.Drawing.Point(569, 198);
+            this.lossesLabel.Name = "lossesLabel";
+            this.lossesLabel.Size = new System.Drawing.Size(107, 51);
+            this.lossesLabel.TabIndex = 8;
+            this.lossesLabel.Text = "Losses: 0";
+            this.lossesLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // tiesLabel
+            // 
+            this.tiesLabel.BackColor = System.Drawing.Color.DarkRed;
+            this.tiesLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tiesLabel.ForeColor = System.Drawing.Color.White;
+            this.tiesLabel.Location = new System.Drawing.Point(569, 268);
+            this.tiesLabel.Name = "tiesLabel";
+            this.tiesLabel.Size = new System.Drawing.Size(107, 51);
+            this.tiesLabel.TabIndex = 9;
+            this.tiesLabel.Text = "Ties: 0";
+            this.tiesLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RockPaperScissors.Properties.Resources.dojo700x390;
+            this.ClientSize = new System.Drawing.Size(684, 351);
+            this.Controls.Add(this.tiesLabel);
+            this.Controls.Add(this.lossesLabel);
+            this.Controls.Add(this.winsLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.scissorsButton);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.chooseLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label chooseLabel;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button scissorsButton;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label winsLabel;
+        private System.Windows.Forms.Label lossesLabel;
+        private System.Windows.Forms.Label tiesLabel;
+    }
+}
+
---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        string playerChoice, cpuChoice;
+        int wins = 0;
+        int losses = 0;
+        int ties = 0;
+
+        Random randGen = new Random();
+        SoundPlayer jabPlayer = new SoundPlayer(Properties.Resources.jabSound);
+        SoundPlayer gongPlayer = new SoundPlayer(Properties.Resources.gong);
+        Image rockImage = Properties.Resources.rock168x280;
+        Image paperImage = Properties.Resources.paper168x280;
+        Image scissorImage = Properties.Resources.scissors168x280;
+        Image winImage = Properties.Resources.winTrans;
+        Image loseImage = Properties.Resources.loseTrans;
+        Image tieImage = Properties.Resources.tieTrans;
+        Point playerLocation = new Point(168, 70);
+        Point cpuLocation = new Point(360, 70);
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        private void rockButton_Click(object sender, EventArgs e)
+        {
+            /// TODO Set the playerchoice value, draw the appropriate image,
+            /// play a sound, wait for a second; repeat for the computer turn 
+        }
+
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scissorsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
